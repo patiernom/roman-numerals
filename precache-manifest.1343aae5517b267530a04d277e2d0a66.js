d503029c81ef2ac5ade7b6c0eecfa7c35d05ae28
patiernom <marco_patierno@msn.com>
2020-10-05 17:51:14 +0200

    Updates

---
+++ precache-manifest.1343aae5517b267530a04d277e2d0a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4439910f028d209b48f9325a062dc8e6",
+    "url": "/roman-numerals/index.html"
+  },
+  {
+    "revision": "f73a6d1124b1835c44e0",
+    "url": "/roman-numerals/static/js/2.4c3a1ed9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/roman-numerals/static/js/2.4c3a1ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775ef722c62bdb0ec5f1",
+    "url": "/roman-numerals/static/js/main.5a0cbbf2.chunk.js"
+  },
+  {
+    "revision": "b98ceb28da145e63635b",
+    "url": "/roman-numerals/static/js/runtime-main.85e70a4f.js"
+  }
+]);
